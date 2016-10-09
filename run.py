804e2e331bca2c4e1d179e333097cfdb4901996e
Thomas Brinkhaus <thomas.brinkhaus@ecg-leipzig.de>
2016-10-09 18:12:39 +0200

    Autostart der Tasks wieder entfernt

--- run.py
+++ run.py
@@ -6,8 +6,5 @@ scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
 
-tasks.checkFollowerForUpdates()
-tasks.retweetAndDeleteTweets()
-
 app.logger.info('Scheduler started, start WebApp')
 app.run(debug=True, host="0.0.0.0", use_reloader=False)
