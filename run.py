bdd34e0bfe4e6e53453f53bd683f61dca06fc60c
Thomas Brinkhaus <thomas.brinkhaus@ecg-leipzig.de>
2016-10-09 17:52:25 +0200

    Start der Jobs beim Startup

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 #!flask/bin/python
-from app import app
+from app import app, tasks
 from flask_apscheduler import APScheduler
 
 scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
+
+tasks.checkFollowerForUpdates()
+tasks.retweetAndDeleteTweets()
 
 app.logger.info('Scheduler started, start WebApp')
 app.run(debug=True, host="0.0.0.0", use_reloader=False)
