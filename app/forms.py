29eae09910720ebb9e5472b6c70ae48118b73c43
Thomas Brinkhaus <thomas.brinkhaus@ecg-leipzig.de>
2016-10-11 11:49:26 +0200

    Unnütze Importe entfernt

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, TextAreaField
+from wtforms import StringField
 from wtforms.validators import DataRequired, Length
 
 class EditAmateurForm(FlaskForm):
